4e654339dd5fa1621f45fb428bc3c3ebf8344309
Andres Diaz <rdiaz13@itfip.edu.co>
2015-06-24 04:28:28 -0500

    Changes
    ef

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,11 +2,11 @@
 
 @section('content')
 <div class="container-fluid">
-    <div class="row">
+    <div class="row" style="margin-top: 50px;">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-                <div class="panel-body">
+                <div class="panel-heading"><b>Login</b></div>
+                <div class="panel-body" style="padding: 30px;">
                     @if (count($errors) > 0)
                         <div class="alert alert-danger">
                             <strong>Whoops!</strong> Hubo Algunos problemas con tu entrada.<br><br>
@@ -19,39 +19,36 @@
                     @endif
 
                     <form class="form-horizontal" role="form" method="POST" action="/auth/login">
-                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
-
-                        <div class="form-group">
-                            <label class="col-md-4 control-label">E-Mail</label>
-                            <div class="col-md-6">
-                                <input type="email" class="form-control" name="email" value="{{ old('email') }}">
-                            </div>
+                        <input type="hidden" name="_token" value="{{ csrf_token() }}">                      
+  
+                        <div class="form-group input-group input-group-lg">
+                            <span class="input-group-addon">Email</span>                            
+                            <input type="email" class="form-control" placeholder="Email" name="email" value="{{ old('email') }}">                            
                         </div>
 
-                        <div class="form-group">
-                            <label class="col-md-4 control-label">Password</label>
-                            <div class="col-md-6">
-                                <input type="password" class="form-control" name="password">
-                            </div>
-                        </div>
+                        <div class="form-group input-group input-group-lg">
+                            <span class="input-group-addon">Password</span>                            
+                            <input type="password" class="form-control" placeholder="Contraseña" name="password" value="{{ old('email') }}">                            
+                        </div>                        
 
                         <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
+                            <div class="col-md-6">
                                 <div class="checkbox">
                                     <label>
-                                        <input type="checkbox" name="remember"> No cerrar sesión
-                                    </label>
-                                </div>
+                                        <input type="checkbox" name="remember">No cerrar sesión
+                                    </label>                                    
+                                </div>                                
+                            </div>
+                            <div class="col-md-6">
+                                <a href="/password/email">¿Olvidaste tu contraseña?</a>
                             </div>
                         </div>
 
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary" style="margin-right: 15px;">
-                                    Login
-                                </button>
-
-                                <a href="/password/email">¿Olvidaste tu contraseña?</a>
+                                <button type="submit" class="btn btn-primary btn-lg">
+                                    Iniciar Sesión
+                                </button>                                
                             </div>
                         </div>
                     </form>
@@ -60,5 +57,4 @@
         </div>
     </div>
 </div>
-</form>
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,52 +1,78 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Laravel</title>
+<head>
+    <title>Thesys - ITFIP</title>
 
-        <link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+    <!-- BOOTSTRAP STYLES-->
+    <link href="{{ '/' }}assets/css/bootstrap.css" rel="stylesheet" />
+    <!-- FONTAWESOME STYLES-->
+    <link href="{{ '/' }}assets/css/font-awesome.css" rel="stylesheet" />
+    <!-- CUSTOM STYLES-->
+    <link href="{{ '/' }}assets/css/custom.css" rel="stylesheet" />
+    <!-- GOOGLE FONTS-->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
 
-        <style>
-            html, body {
-                height: 100%;
-            }
+    <style>
+        html, body {
+            height: 100%;
+        }
 
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                color: #B0BEC5;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
+        body {
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            color: #B0BEC5;
+            display: table;
+            font-weight: 100;
+            font-family: 'Lato';
+        }
 
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
+        .container {
+            text-align: center;
+            display: table-cell;
+            vertical-align: middle;
+        }
 
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
+        .content {
+            text-align: center;
+            display: inline-block;
+        }
 
-            .title {
-                font-size: 96px;
-                margin-bottom: 40px;
-            }
+        .title {
+            font-size: 96px;
+            margin-bottom: 40px;
+        }
 
-            .quote {
-                font-size: 24px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Thesys</div>
-                <div class="quote">{{ Inspiring::quote() }}</div>
+        .logo img{
+            width: 50%;
+            min-width: 250px;
+            padding: 2em;
+        }
+
+        .quote{
+            color: #6F6F6F;
+            font-style: italic;
+            font-size: 24px;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="content">
+            <div class="logo">
+                <img src="{{ '/' }}assets/img/logo.png">
             </div>
-        </div>
-    </body>
-</html>
+            <div class="col-md-4 col-md-offset-3">
+                <a href="{{ '/' }}auth/login"><img src="{{ '/' }}assets/img/id.png">
+                    <h3>Iniciar Sesión</h3></a>                                                    
+                </div>
+                <div class="col-md-4 col-lg-2">
+                    <a href="{{ '/' }}search"><img src="{{ '/' }}assets/img/search.png">
+                        <h3>Buscar Tesis</h3></div></a>                                                   
+                    </div>
+
+                    <div class="quote">{{ Inspiring::quote() }}</div>
+                </div>
+            </div>
+        </body>
+        </html>
