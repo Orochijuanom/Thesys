666a20bb053698cea8cf516d1beedf82676930b7
Juan Sebastian Cruz Perdomo <jcruz02@itfip.edu.co>
2015-07-16 01:51:20 -0500

    comite

--- app/Http/Controllers/Comite/LoginController.php
+++ app/Http/Controllers/Comite/LoginController.php
@@ -7,79 +7,71 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
+use View;
+use App\Classes\Rest;
+use Redirect;
+
+use App\Comite;
+
 class LoginController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return Response
-     */
-    public function index()
+    public function __construct()
     {
-        //
+    
+      $this->middleware('guest');
+    
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return Response
-     */
-    public function create()
-    {
-        //
+    public function getIndex(){
+
+        return View::make('comite.auth.login');
+
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @return Response
-     */
-    public function store()
-    {
-        //
-    }
+    public function postIndex(Request $request){
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function show($id)
-    {
-        //
-    }
+        $this->validate($request, [
+            
+            'username' => 'required|min:4',
+            'password' => 'required|min:4',
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function edit($id)
-    {
-        //
-    }
+            ]);
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function update($id)
-    {
-        //
-    }
+        
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+        $rest = new Rest();
+        $response = $rest->CallAPI('POST', 'http://ryca.itfip.edu.co:8888/profesor/login', 
+          [
+
+            'usuario' => strtoupper($request['username']),
+            'clave' => $request['password'],
+            
+          ]);
+
+        $usuario = json_decode($response);
+
+        if($usuario->error == true){
+
+            return Redirect::back()->withInput()->withErrors(['mesagge' => 'Los datos no coinciden con nuestros registros.']);
+
+        }
+
+        try {
+            
+           $user = Comite::where('cod_user_ryca', $usuario->user->id)->firstOrFail();
+
+        } 
+        catch (ModelNotFoundException $e) {
+            
+          return Redirect::back()->withInput()->withErrors(['mesagge' => 'El usuario no se encuentra como asignado como miembro del comite curricular en el sistema']);
+        }
+
+          //valida si se devulve error de la consulta, en cuyo caso no hubo loggin
+         
+          
+          session()->put(['user.name' => $usuario->user->nombres.' '.$usuario->user->apellidos, 'user.token' => $usuario->token, 'user.tipo' => 'comite', 'user.programa' => $user['cod_prog_ryca'], 'user.foto' => $usuario->user->foto]);
+
+          return Redirect::to('/comite/home');  
+
+        }
 }
