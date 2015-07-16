07d01a5034a8b13a6192f2044f8f7ebb91351c61
Andres Diaz <rdiaz13@itfip.edu.co>
2015-07-16 17:01:20 -0500

    rg
    rgr

--- resources/views/estudiante/tesis/index.blade.php
+++ resources/views/estudiante/tesis/index.blade.php
@@ -5,7 +5,7 @@
 
 <ul class="nav" id="main-menu">
     <li>
-        <a href="{{ '/' }}estudiante/tesis" class="active-menu"><i class="fa fa-list fa-3x"></i>Trabajo de Grado</a>
+        <a href="{{ '/' }}estudiante/tesis" class="active-menu"><i class="fa fa-book fa-3x"></i>Trabajo de Grado</a>
     </li>
     
 </ul>
@@ -21,85 +21,86 @@
 
         <div class="container-fluid">
             <div class="row">                
-                    <div class="panel panel-primary">
-                        <div class="panel-heading title-caja">Trabajo de Grado</div>
-                        <div class="panel-body" style="padding: 30px;">
-                            @if (Session::get('mensagge_delete'))
-                            <div class="alert alert-success">
-                                {{Session::get('mensagge_delete')}}
-                                <br><br>            
-                            </div>
-                            @endif
+                <div class="panel panel-primary">
+                    <div class="panel-heading title-caja">Trabajo de Grado</div>
+                    <div class="panel-body" style="padding: 30px;">
+                        @if (Session::get('mensagge_delete'))
+                        <div class="alert alert-success">
+                            {{Session::get('mensagge_delete')}}
+                            <br><br>            
+                        </div>
+                        @endif
 
-                            @if (count($errors) > 0)
-                            <div class="alert alert-danger">
-                                <strong>Whoops!</strong> Hubo Algunos problemas con tu entrada.<br><br>
-                                <ul>
-                                    @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
+                        @if (count($errors) > 0)
+                        <div class="alert alert-danger">
+                            <strong>Whoops!</strong> Hubo Algunos problemas con tu entrada.<br><br>
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                        @endif
+
+                        @if(count($estudiante['tesis'])>0)
+
+                        <div class="table-responsive">
+                            <table class="table table-striped table-bordered table-hover">
+                                <thead>
+                                    <tr>
+                                        <th>Titulo</th>
+                                        <th>Director</th>
+                                        <th>Estado</th>
+                                        <th>Archivo</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+
+                                    @foreach ($estudiante->tesis as $tesis)
+
+                                    @if(session()->get('user.programa') == $tesis['cod_prog_ryca'])
+                                    <tr>
+                                        <td data-title='titulo' style="max-width:450px"><a href="estudiante/tesis/{{$tesis->id}}">{{$tesis['titulo']}}</td></a>                 
+                                        <td data-title='Profesor'>{{$profesores[$tesis['director_cod_user_ryca']]}}</td>
+                                        @foreach($estados as $estado)
+
+                                        @if($estado['id'] == $tesis['estado_id'])
+
+                                        <td data-title='Estado'>{{$estado['estado']}}</td>
+
+                                        @endif
+
+                                        @endforeach
+
+                                        <td data-title='archivo'>
+                                        <a href="{{ '/' }}{{$tesis->source}}" class="btn btn-success btn-circle"><i class="fa fa-download fa-3x"></i></a>
+                                        </td>
+                                    </tr>
+
+                                    @else
+
+                                    <p class='alert alert-info'><strong>Whoops!</strong> No se encuetran Tesis en el sistema.</p>
+
+                                    @endif
+
                                     @endforeach
-                                </ul>
-                            </div>
-                            @endif
+                                </tbody>
 
-                            @if(count($estudiante['tesis'])>0)
-                           
-                                <div class="table-responsive">
-                                    <table class="table table-striped table-bordered table-hover">
-                                        <thead>
-                                            <tr>
-                                                <th>Titulo</th>
-                                                <th>Director</th>
-                                                <th>Estado</th>
-                                                <th>Archivo</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            
-                                            @foreach ($estudiante->tesis as $tesis)
-                                            
-                                                @if(session()->get('user.programa') == $tesis['cod_prog_ryca'])
-                                                    <tr>
-                                                        <td data-title='titulo' style="max-width:450px"><a href="estudiante/tesis/{{$tesis->id}}">{{$tesis['titulo']}}</td></a>                 
+                            </table>
+                        </div>                                
 
-                                                        <td data-title='Profesor'>{{$profesores[$tesis['director_cod_user_ryca']]}}</td>
-                                                        @foreach($estados as $estado)
+                        @else
 
-                                                            @if($estado['id'] == $tesis['estado_id'])
-                                                                
-                                                                <td data-title='Estado'>{{$estado['estado']}}</td>
-                                                                
-                                                            @endif
+                        <p class='alert alert-info'><strong>Whoops!</strong> No se encuetran Tesis en el sistema.</p>
 
-                                                        @endforeach
-                                                        
-                                                        <td data-title='archivo'><a href="{{ '/' }}{{$tesis->source}}" target="_blank" >File</a></td>
-                                                    </tr>
+                        @endif
 
-                                                @else
-
-                                                    <p class='alert alert-info'><strong>Whoops!</strong> No se encuetran Tesis en el sistema.</p>
-
-                                                @endif
-
-                                            @endforeach
-                                        </tbody>
-
-                                    </table>
-                                </div>                                
-                            
-                            @else
-
-                            <p class='alert alert-info'><strong>Whoops!</strong> No se encuetran Tesis en el sistema.</p>
-
-                            @endif
-
-                            <div class="col-md-5 col-md-offset-5">
+                        <div class="col-md-5 col-md-offset-5">
                             <a href="{{url('estudiante/tesis/create')}}" class="btn btn-primary btn-lg"><i class="fa fa-plus"> Añadir Tesis</i></a>
                         </div>
-                            
-                        </div>
-                    </div> 
+
+                    </div>
+                </div> 
             </div>
         </div>
     </div>
